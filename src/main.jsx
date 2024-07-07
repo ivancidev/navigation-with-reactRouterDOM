6bd52fdcaaa219efcb1dae398875f2e14332f804
Herlan <herlanherbas@gmail.com>
2024-07-07 01:09:05 -0400

    add errorPage and UserProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,44 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './routes/Root'
-import Home from './pages/Home'
-import About from './pages/About'
-import Blog from './pages/Blog'
-import React from 'react'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/Root";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import React from "react";
+import ErrorPage from "./pages/ErrorPage";
+import UserProvider from "./context/UserProvider";
 
 const router = createBrowserRouter([
   {
-    path: '/',  
+    path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: '/about',
-        element: <About />
+        path: "/about",
+        element: <About />,
       },
       {
-        path: '/blog',
-        element: <Blog />
-      }
-    ]
+        path: "/blog",
+        element: <Blog />,
+      },
+    ],
   },
-])
+  // Con path dinamico
+  // {
+  //   path: '/blog/:id',
+  //   element: <Blog />
+  // }
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  </React.StrictMode>
+);
