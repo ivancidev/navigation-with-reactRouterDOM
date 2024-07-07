c60d00e8cdeeed1e2207d436103dab470c1ecff2
Herlan <herlanherbas@gmail.com>
2024-07-07 01:11:37 -0400

    add component Table and useContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import Table from "../components/Table/Table";
+import { UserContext } from "../context/UserContext";
 
 export default function Home() {
+  const { user } = useContext(UserContext);
   return (
-    <div>Home</div>
+    <>
+    <Table user={user}  />
+    </>
   )
 }
