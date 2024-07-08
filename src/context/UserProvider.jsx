6880cd4865d3a35b0971ede73e160a3e69cca9fd
Herlan <herlanherbas@gmail.com>
2024-07-08 10:45:41 -0400

    add state user

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,16 +1,11 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
 
-const user = {
-    id: 1,
-    name: "Marco",
-    lastName: "Rocha",
-    email: "marco@gmail.com",
-    country: "Colombia"
-}
 
 export default function UserProvider({ children }) {
+  const [user, setUser] = useState({});
   return (
-    <UserContext.Provider value={{ user }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   )
