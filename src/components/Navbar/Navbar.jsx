9cb5a03aa4fe2b9b12346342dcff2da2b80be520
Herlan <herlanherbas@gmail.com>
2024-07-08 10:17:41 -0400

    add styles and state hover

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,42 @@
 import { Link } from "react-router-dom";
 import "./styles/navbar.css";
+import { useState } from "react";
 
 export default function Navbar() {
+  const param = window.location.pathname;
+  const [hover, setHover] = useState(param);
+
+  const handleHover = (param) => setHover(param);
+
   return (
     <nav>
       <ul>
         <li>
-            <Link to={"/"}>Home</Link>
+          <Link
+            to={"/"}
+            style={{ color: hover == "/" ? "blue" : "" }}
+            onClick={() => handleHover("/")}
+          >
+            Home
+          </Link>
         </li>
         <li>
-            <Link to={"/about"}>About</Link>
+          <Link
+            to={"/about"}
+            style={{ color: hover == "/about" ? "blue" : "" }}
+            onClick={() => handleHover("/about")}
+          >
+            About
+          </Link>
         </li>
         <li>
-            <Link to={"/blog"}>Blog</Link>
+          <Link
+            to={"/blog"}
+            style={{ color: hover == "/blog" ? "blue" : "" }}
+            onClick={() => handleHover("/blog")}
+          >
+            Blog
+          </Link>
         </li>
       </ul>
     </nav>
